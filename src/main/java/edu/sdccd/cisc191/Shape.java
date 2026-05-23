6907a4d7ead9cc51a22f1edc9fcf9d0950eb4d0c
hannah <htonnu001@student.sdccd.edu>
2026-05-23 00:18:56 -0700

    added measurable interface

--- src/main/java/edu/sdccd/cisc191/Shape.java
+++ src/main/java/edu/sdccd/cisc191/Shape.java
@@ -1,34 +1,33 @@
 package edu.sdccd.cisc191;
 
-public abstract class Shape implements Comparable<Shape> {
+public abstract class Shape implements Comparable<Shape>, Measurable {
+
     protected String name;
 
     public Shape(String name) {
-        if (name == null || name.isEmpty()) {
 
-            throw new IllegalArgumentException("Name cannot be null/empty");
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
         }
 
-        this.name = name;
+        this.name = name.trim();
     }
 
     public String getName() {
-
         return name;
     }
 
+    @Override
     public abstract double area();
 
     public abstract double perimeter();
 
     public String describe() {
-
         return getClass().getSimpleName() + "[" + name + "]";
     }
 
     @Override
     public int compareTo(Shape other) {
-
         return this.name.compareTo(other.name);
     }
 
